9cd51742dbded8196c228604b590bcbc20b211ff
GreatHub1204 <grahasmin@gmail.com>
2023-02-17 02:45:14 -0500

    uses package

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/generate-result','App\Http\Controllers\PromptController@generateResult');
