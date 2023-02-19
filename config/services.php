4cd438ef811a2cdf756118f96bd7aaecbfd64128
GreatHub1204 <grahasmin@gmail.com>
2023-02-19 17:13:54 -0500

    url

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'https://0r1kul.jcompsolu.com/auth/callback',
+        'redirect' => url('/auth/callback'),
     ],
 
 ];
