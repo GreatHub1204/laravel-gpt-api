923813dddd57ed94e6ad3e0c79d9489e147a1017
GreatHub1204 <grahasmin@gmail.com>
2023-02-19 17:18:51 -0500

    REDIRECT

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => url('/auth/callback'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
     ],
 
 ];
