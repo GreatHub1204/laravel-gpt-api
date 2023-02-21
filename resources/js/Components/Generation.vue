0d8455e1323a423f198689db160f5e88a1c9599d
GreatHub1204 <grahasmin@gmail.com>
2023-02-20 23:52:48 -0500

    testing

--- resources/js/Components/Generation.vue
+++ resources/js/Components/Generation.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, computed, reactive } from 'vue'
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { Link, useForm } from '@inertiajs/vue3';
 import TextInput from '@/Components/TextInput.vue';
@@ -11,16 +12,49 @@ import axios from 'axios';
 const form = useForm({
     prompt: '',
     type: 'image',
-    imageUrl: '',
-    processing: false
+    processing: false,
+    resultText: '',
+    imageUrl: ''
 });
+async function shareImage(url) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const filesArray = [
+    new File(
+      [blob],
+      new Date().getTime() + '.jpg',
+      {
+        type: "image/png",
+        lastModified: new Date().getTime()
+      }
+   )
+  ];
+  const shareData = {
+    files: filesArray,
+  };
+  navigator.share(shareData);
+}
 const submit = () => {
   // axios.defaults.withCredentials = true;
+  let data = {}
+  if (form.type == 'image') {
+    data = {prompt:form.prompt, type: form.type}
+  } else {
+    data = {prompt:form.prompt, type: form.type, model: 'text-davinci-001', max_tokens: 1000, temperature: 0.9}
+  }
   form.processing = true
-   axios.post('/api/generate-result', {prompt:form.prompt, type: form.type})
+  form.reset('imageUrl')
+  form.reset('resultText')
+   axios.post('/api/generate-result', data)
    .then(res => {
      form.processing = false
-     form.imageUrl = res.data.data.data.data[0].url
+     if (form.type === 'image') {
+       form.imageUrl = res.data.data.data.data[0].url
+     } else {
+       form.resultText = res.data.data.data.choices[0].text
+     }
+
+     form.reset('prompt')
    })
     // form.transform(data => ({
     //     ...data,
@@ -34,7 +68,7 @@ const submit = () => {
   <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 ">
     <FormSection @submitted="submit">
       <template #title>
-        <h3>Generation Form</h3>
+        <h3 class="text-gray-500">Generation Form</h3>
       </template>
       <template #description>
         <p>
@@ -69,10 +103,15 @@ const submit = () => {
                 required
                 autofocus
             />
-            <InputError class="mt-2" :message="form.errors.prompt" />
         </div>
       </template>
     </FormSection>
+    <p v-show="form.resultText !== ''" class="mt-6 text-gray-500 leading-relaxed">
+      {{form.resultText}}
+    </p>
     <img loading="lazy" v-show="form.imageUrl !== ''" :src="form.imageUrl" class="object-contain h-96 w-96 rounded"/>
+    <!-- <PrimaryButton @click="shareImage(form.imageUrl)" v-show="form.imageUrl !== ''">
+      Share Image
+    </PrimaryButton> -->
   </div>
 </template>
